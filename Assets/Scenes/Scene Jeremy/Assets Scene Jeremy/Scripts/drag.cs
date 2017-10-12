4d4d8fdad139e1c9e2ef38848cf292a4bd40f24d
DESKTOP-859FG5E\jeremy <jeremy.real@e-artsup.net>
2017-10-12 23:31:07 +0200

    Drag and drop + début add force

---
+++ Assets/Scenes/Scene Jeremy/Assets Scene Jeremy/Scripts/drag.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Drag
+public class drag : MonoBehaviour {
+	float distance = 5;
+	void OnMouseDrag(){
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		transform.position = objPosition;
+	}
+}
+	
