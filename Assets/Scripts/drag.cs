2a337038e4595fccf892f5bfec995f7dc16b49f7
DESKTOP-S0E0I8R\Kévin <kevin.arenas@outlook.fr>
2017-10-13 12:21:42 +0200

    addforce

--- Assets/Scripts/drag.cs
+++ Assets/Scripts/drag.cs
@@ -1,13 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 //Drag
 public class drag : MonoBehaviour {
+	
 	float distance = 5;
-	void OnMouseDrag(){
+	 Vector3 mMouseDownPos;
+ 	Vector3 mMouseUpPos;
+ 	public float speed = .1f;
+ 
+	
+
+
+	void OnMouseDrag()
+	{
+
 		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
 		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
 		transform.position = objPosition;
 	}
+
+
+ 
+ 	void OnMouseDown() 
+ 		{
+     
+    		 mMouseDownPos = Input.mousePosition;
+   			 Debug.Log( "the mouse down pos is " + mMouseDownPos.y.ToString() );
+   			 mMouseDownPos = Input.mousePosition;
+    		 Debug.Log( "the mouse down pos is " + mMouseDownPos.z.ToString() );
+     		 mMouseDownPos.z = 0;
+	 	}
+ 
+ 	void OnMouseUp() 
+ 		{
+
+     		mMouseUpPos = Input.mousePosition;
+    		mMouseUpPos = Input.mousePosition;
+     		mMouseUpPos.z = 0;
+     		var direction = mMouseDownPos - mMouseUpPos;
+     		direction.Normalize();
+     		GetComponent<Rigidbody>().AddForce (-direction * speed,ForceMode.Impulse);
+     		Debug.Log( "the mouse up pos is " + mMouseUpPos.ToString() );
+ 		}
 }
 	
