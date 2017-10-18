102de86d94c337467bc85714beb67fed99a30ea0
Arthur Manoux <arthur.manoux@wanadoo.fr>
2017-10-18 17:59:40 +0200

    mise a jour planning + script holes

---
+++ Assets/Scripts/ScriptArthur/TrouMur.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrouMur : MonoBehaviour {
+
+public int numHoles;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void UpdateHoles()
+	{
+		numHoles--;
+		if(numHoles == 0)
+		{
+			Debug.Log("Changementdemur");
+
+		}
+		
+		else if(numHoles != 0)
+		{
+		
+			UpdateHoles();
+		
+		}
+	}
+}
