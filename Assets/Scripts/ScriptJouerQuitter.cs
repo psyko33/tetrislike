68ec35f664778f13bdc159bdd67d058fd088432e
Mikael.PC <mikaeljouchoux@gmail.com>
2017-10-18 16:56:59 +0200

    Script lancement scene, lobby , game,  quit

---
+++ Assets/Scripts/ScriptJouerQuitter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptJouerQuitter : MonoBehaviour 
+{
+
+
+	public void OnClickStart ()
+	{
+	 Application.LoadLevel("SceneLobby");	
+	}
+	
+	public void OnClickStart2 ()
+	{
+	 Application.LoadLevel("SceneMikael");	
+	}
+	
+
+	public void QuitButton()
+	{
+		Application.Quit() ;
+	}
+
+}
