999e5333fe479a76e86ab69cf048839a953ca5bd
DESKTOP-859FG5E\jeremy <jeremy.real@e-artsup.net>
2017-10-18 16:40:28 +0200

    Pause manager + suppresion death

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+
+ void Update()
+ {
+     if (Input.	GetKeyDown(KeyCode.P))
+     {
+         if (Time.timeScale == 1)
+         {
+             Time.timeScale = 0;
+         }
+         else
+         {
+             Time.timeScale = 1;
+         }
+     }
+ }
+
+ 							}
