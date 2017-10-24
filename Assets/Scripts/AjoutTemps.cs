18aae6bb34a10e2b33cba3d967a50c0a8dd85290
DESKTOP-859FG5E\jeremy <jeremy.real@e-artsup.net>
2017-10-24 16:58:33 +0200

    script boutique

---
+++ Assets/Scripts/AjoutTemps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AjoutTemps : MonoBehaviour {
+
+public GameObject tableaux;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		tableaux.GetComponent<Tableaux>().AddTimer();
+	}
+}
